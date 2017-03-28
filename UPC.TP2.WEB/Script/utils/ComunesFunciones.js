6db650c905d0e03ac79167a4beec39a2cbffd95a
fsanchezs <f.sanchez630@gmail.com>
2017-03-28 16:49:12 -0500

    https://github.com/fsanchezs/clinicaRicardoPalma

---
+++ UPC.TP2.WEB/Script/utils/ComunesFunciones.js
@@ -0,0 +1,44 @@
+﻿//=== Valida fecha Inicio / Fin
+
+$('#form_generar_servicio_salud').on('submit', function (e) {
+
+    var fi = $("#fecha_inicio").val();//$.datepicker.formatDate("yyyy/mm/dd", $("#fecha_inicio").val());//$("#fecha_inicio").val();
+    var ff = $("#fecha_fin").val();//$.datepicker.formatDate("yyyy/mm/dd", $("#fecha_fin").val());//$("#fecha_fin").val();
+
+});
+
+
+$('#submit_retirar').on('click', function (e) {
+    $('#accion').attr('value', 'retirar');
+    var min = 0;
+    $("#table_retirar").find("input[type='checkbox']").each(function () {
+        if ($(this).is(":checked")) {
+            min++;
+        }
+    });
+    if (min > 0) {
+        $("#form_generar_accion").submit();
+    }
+    else {
+        alert("Debe seleccionar almenos una fila");
+        e.preventDefault();
+    } 
+});
+
+$("#submit_asignar").on("click", function (e) {
+    $("#accion").attr("value", "asignar");
+    var min = 0;
+    $("#table_asignar").find("input[type='checkbox']").each(function () {
+        if ($(this).is(":checked")) {
+            min++;
+        }
+    });
+    if (min > 0) {
+        $("#form_generar_accion").submit();
+    }     
+    else {
+        alert("Debe asignar almenos una fila");
+        e.preventDefault();
+    }
+        
+});
---
+++ UPC.TP2.WEB/ViewModels/GenerateServiceModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UPC.TP2.WEB.Models;
+
+namespace UPC.TP2.WEB.ViewModels
+{
+    public class GenerateServiceModel
+    {
+        public IEnumerable<T_PROGRAMACION_MEDICA> LIST_RETIRO_SERVICIO { get; set; }
+        public IEnumerable<T_PROGRAMACION_MEDICA> LIST_ASIGNA_PLAN { get; set; }
+        public IEnumerable<T_PLAN_DE_SALUD> LIST_PLAN_DE_SALUD { get; set; }
+        public T_CONFIGURACION T_CONFIGURACION_RETIRAR { get; set; }
+        public T_CONFIGURACION T_CONFIGURACION_ASIGNAR { get; set; }
+    }
+}
